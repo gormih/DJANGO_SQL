b72eef697b29b4e33917943d30954eeb4ce0d993
Mikhai Goryunov <gormih78@yandex.ru>
2019-12-13 16:52:32 +0300

    First commit

---
+++ sql_execute/views.py
@@ -0,0 +1,30 @@
+# from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
+from django.db.utils import OperationalError
+from django.db import connection
+
+
+def sql_execute(request):
+    if request.user.is_staff:
+
+        if request.method == 'GET':
+            return render(request, 'sql.html')
+        elif request.method == 'POST':
+            query = request.POST['query']
+            cursor = connection.cursor()
+            try:
+                cursor.execute(query)
+            except OperationalError as error:
+                context = {'error': error.message,
+                           'query': query, }
+                return render(request, 'sql.html', context)
+            data = cursor.fetchall()
+            # cursor.execute(query)
+            desc = cursor.description
+            headers = [d[0] for d in desc]
+            context = {'data': data,
+                       'query': query,
+                       'headers': headers}
+            return render(request, 'sql.html', context)
+    else:
+        return HttpResponse('auth failed!')
